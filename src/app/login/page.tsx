0c5b5fbdfc60bbf9f6ce94d107fb9a87627e577e
Mohamed Gad <mohamedgad149@gmail.com>
2024-04-17 00:51:48 +0200

    login and sign up pages -

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+import { LoginPage } from "@/components/login";
+
+export const ServerLoginPage = () => {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+};
+export default ServerLoginPage;
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,6 @@
+import { SignUpPage } from "@/components/signup";
+
+const ServerSignUpPage = () => {
+  return <SignUpPage />;
+};
+export default ServerSignUpPage;
