13216778e34b8c91bbf842b18e11e333a6549454
Mohamed Gad <mohamedgad149@gmail.com>
2024-04-17 01:03:40 +0200

    login bg

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -1,3 +1,7 @@
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className=" h-svh w-screen bg-loginBg bg-no-repeat bg-cover bg-center flex items-center justify-center">
+      <div className="md:w-[500px] w-[calc(100%-30px)] rounded-lg overflow-hidden bg-texture bg-no-repeat bg-cover bg-center min-h-96 "></div>
+    </div>
+  );
 };
