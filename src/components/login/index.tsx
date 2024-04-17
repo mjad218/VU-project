9d057029875f3917ef0b56dd5342bbcac080ba38
Mohamed Gad <mohamedgad149@gmail.com>
2024-04-17 02:35:34 +0200

    fix z-index

--- src/components/login/index.tsx
+++ src/components/login/index.tsx
@@ -5,7 +5,7 @@ import { TextureBackground } from "../texture-wrapper";
 export const LoginPage = () => {
   return (
     <div className=" h-svh w-screen bg-loginBg bg-no-repeat bg-cover bg-center flex items-center justify-center">
-      <TextureBackground className="md:w-[500px] w-[calc(100%-30px)] rounded-lg min-h-96 flex flex-col justify-between px-4 py-6">
+      <TextureBackground className="md:w-[500px] w-[calc(100%-30px)] rounded-lg min-h-96 flex flex-col justify-between px-4 py-6 -z-1">
         <LoginForm />
 
         <span className=" self-end text-[var(--brown)] text-base flex gap-2 items-center">
--- src/components/texture-wrapper/index.tsx
+++ src/components/texture-wrapper/index.tsx
@@ -5,16 +5,27 @@ type IProps = {
   children?: ReactNode | undefined;
   className?: string | undefined;
 };
+
 export const TextureBackground = (props: IProps) => {
   return (
     <div
       className={cn(
-        "overflow-hidden bg-texture bg-no-repeat bg-cover bg-center relative ",
-        props.className
+        "overflow-hidden bg-[var(--beige)] relative ",
+        props.className,
+        "p-0 px-0 py-0"
       )}
     >
-      <div className="h-full w-full bg-black opacity-20 flex-1   absolute top-0 right-0 left-0 bottom-0" />
-      {props.children}
+      <div className="h-full bg-texture bg-no-repeat bg-cover bg-center w-full opacity-20 flex-1 absolute top-0 right-0 left-0 bottom-0 z-0" />
+
+      <div
+        className={cn(
+          "overflow-hidden bg-[var(--beige)] relative  ",
+          props.className,
+          "w-full h-full bg-transparent"
+        )}
+      >
+        {props.children}
+      </div>
     </div>
   );
 };
